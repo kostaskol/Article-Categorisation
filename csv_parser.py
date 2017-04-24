c0c42625b937ce5781506a84b09d34c77104d68b
Kostas Kolivas <kwstaskolivas@gmail.com>
2017-04-24 20:54:31 +0300

    Nothing more works

--- csv_parser.py
+++ csv_parser.py
@@ -2,49 +2,16 @@ from pandas import read_csv
 import numpy
 from sklearn.feature_extraction.text import CountVectorizer, ENGLISH_STOP_WORDS
 from sklearn.naive_bayes import MultinomialNB
-w = 5
-word_cloud = [[] for x in range(w)]
-cvs = []
+from wordcloud import WordCloud
+import re
+from stop_words import get_stop_words
+from sklearn import svm
 
-'''
-cv = CountVectorizer(min_df=0, charset_error="ignore",
-                         stop_words="english", max_features=200)
-
-counts = cv.fit_transform([text]).toarray().ravel()
-words = np.array(cv.get_feature_names())
-# normalize
-counts = counts / float(counts.max())
-'''
-'''
-A = numpy.array(df.head(1000))
-
-for i in range(A.shape[0]):
-    t = str(A[i][4])
-    if t == 'Politics':
-        if len(word_cloud[0]) == 0:
-            word_cloud[0].append('Politics')
-        word_cloud[0].append(str(A[i][3]))
-    elif t == 'Film':
-        if len(word_cloud[1]) == 0:
-            word_cloud[1].append('Film')
-        word_cloud[1].append(str(A[i][3]))
-    elif t == 'Football':
-        if len(word_cloud[2]) == 0:
-            word_cloud[2].append('Football')
-        word_cloud[2].append(str(A[i][3]))
-    elif t == 'Business':
-        if len(word_cloud[3]) == 0:
-            word_cloud[3].append('Business')
-        word_cloud[3].append(str(A[i][3]))
-    elif t == 'Technology':
-        if len(word_cloud[4]) == 0:
-            word_cloud[4].append('Technology')
-        word_cloud[4].append(str(A[i][3]))
-        '''
+stop_word = get_stop_words('en')
 
 
 class Document:
-    def __init__(self, csv_file):
+    def __init__(self, csv_file, head):
         self.csv = csv_file
         self.target_names = {
             0: 'Politics',
@@ -53,12 +20,13 @@ class Document:
             3: 'Business',
             4: 'Technology'
         }
+
         self.data = []
         self.target = []
 
         df = read_csv('train_set.csv', sep='\t')
 
-        A = numpy.array(df.head(5000))
+        A = numpy.array(df.head(head))
 
         for i in range(A.shape[0]):
             t = str(A[i][4])
@@ -74,19 +42,77 @@ class Document:
             elif t == 'Technology':
                 self.target.append(4)
 
+    def get_text(self, target):
+        string = ""
+        for i in range(len(self.data)):
+            if self.target[i] == target:
+                string += self.data[i]
 
-doc = Document('train_set.csv')
+        return string
 
-count_vect = CountVectorizer(stop_words=ENGLISH_STOP_WORDS)
-count_vect.fit(doc.data)
-X_train_counts = count_vect.transform(doc.data)
+    def get_clean(self, target):
+        string = self.get_text(target)
+        global stop_word
+        for i in stop_word:
+            tmp = re.compile(re.escape(' ' + i + ' '), re.IGNORECASE)
+            string = tmp.sub(' ', string)
 
-clf = MultinomialNB().fit(X_train_counts, doc.target)
+        return string
 
-docs_new = ['We are programming in python', 'Money']
-X_new_counts = count_vect.transform(docs_new)
 
-predicted = clf.predict(X_new_counts)
+def bayes(head):
+    doc = Document('train_set.csv', head)
 
-for docs, category in zip(docs_new, predicted):
-    print('%r => %s' % (docs, doc.target_names[category]))
+    count_vect = CountVectorizer(stop_words=stop_word)
+    count_vect.fit(doc.data)
+    X_train_counts = count_vect.transform(doc.data)
+
+    clf = MultinomialNB().fit(X_train_counts, doc.target)
+
+    docs_new = ['We are programming in python', 'Money']
+    X_new_counts = count_vect.transform(docs_new)
+
+    predicted = clf.predict(X_new_counts)
+
+    for docs, category in zip(docs_new, predicted):
+        print('%r => %s' % (docs, doc.target_names[category]))
+
+
+def svm_clf(head):
+    doc = Document('train_set.csv', head)
+
+    cv = CountVectorizer(stop_words=stop_word)
+    cv.fit(doc.data)
+    x_train_counts = cv.transform(doc.data)
+
+    clf = svm.SVC(decision_function_shape='ovo')
+    clf.fit(x_train_counts, doc.target)
+
+    dec = clf.decision_function([[1]])
+    print(dec.shape)
+
+    docs_new = ['We are programming in python', 'Money']
+    x_new_counts = cv.transform(docs_new)
+
+    predicted = clf.predict(x_new_counts)
+
+    for docs, category in zip(docs_new, predicted):
+        print('%r => %s' % (docs, doc.target_names[category]))
+
+
+def word_cloud(tar):
+    doc = Document('train_set.csv')
+
+    wordcloud = WordCloud().generate(doc.get_clean(tar))
+
+    import matplotlib.pyplot as plt
+    plt.imshow(wordcloud, interpolation='bilinear')
+    plt.axis("off")
+
+    plt.show()
+
+
+def k_means(hear):
+    pass
+
+svm_clf(5000)
