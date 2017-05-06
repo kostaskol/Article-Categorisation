0dd3de9b81b8e4ceefc8acdac0308d06f33a7af0
Kostas Kolivas <kwstaskolivas@gmail.com>
2017-05-06 21:03:19 +0300

    Wordcloud works after split

---
+++ Document.py
@@ -0,0 +1,71 @@
+from pandas import read_csv
+import numpy as np
+from nltk.stem.porter import PorterStemmer
+from stop_words import get_stop_words
+from nltk.tokenize import wordpunct_tokenize as tokenize
+import string as String
+
+stop_word = get_stop_words('english')
+
+class Document:
+    def __init__(self, csv_file, start=0, end=-1):
+        self.csv = csv_file
+        self.target_names = {
+            0: 'Politics',
+            1: 'Film',
+            2: 'Football',
+            3: 'Business',
+            4: 'Technology'
+        }
+
+        self.data = []
+        self.target = []
+
+        df = read_csv('train_set.csv', sep='\t')
+
+        if start == 0 and end != -1:
+            df = df.head(end)
+        elif start == 0 and end == -1:
+            pass
+        elif start != 0 and end != -1:
+            df = df.iloc[start: end]
+        elif start != 0 and end == -1:
+            df = df.iloc[start: df.size]
+
+        a = np.array(df)
+
+        for i in range(a.shape[0]):
+            t = str(a[i][4])
+            self.data.append(str(a[i][3]))
+            if t == 'Politics':
+                self.target.append(0)
+            elif t == 'Film':
+                self.target.append(1)
+            elif t == 'Football':
+                self.target.append(2)
+            elif t == 'Business':
+                self.target.append(3)
+            elif t == 'Technology':
+                self.target.append(4)
+
+    def get_text(self, target):
+        string = ""
+        for i in range(len(self.data)):
+            if self.target[i] == target:
+                string += self.data[i]
+
+        return string
+
+    def get_clean(self, target):
+        
+        oth_st_words = ['said']
+        for oth_word in oth_st_words:
+            stop_word.append(oth_word)
+
+        s = self.get_text(target)
+        for word in stop_word:
+            s = s.replace(' ' + word + ' ', "")
+            s = s.replace(',' + word + ' ', "")
+            s = s.replace(' ' + word + '.', "")
+
+        return s 
